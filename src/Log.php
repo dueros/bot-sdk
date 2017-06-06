20ef09e4cbe0ba9dc26c0943c5b2db8499828445
yuanpeng <yuanpeng01@baidu.com>
2017-06-06 18:54:04 +0800

    update readme
    Change-Id: I74a3596900c9000e650d60b1e78b632949c79fee

--- src/Log.php
+++ src/Log.php
@@ -33,7 +33,7 @@ class Log{
         $this->_in_line=$options['in_line'];
         $this->options=$options;
 
-        $this->logid = $_SERVER['HTTP_SAIYALOGID'] ? $_SERVER['HTTP_SAIYALOGID']:'';
+        $this->logid = $_SERVER['HTTP_SAIYALOGID'] ? $_SERVER['HTTP_SAIYALOGID']:''.time().mt_rand(1000, 10000);
         date_default_timezone_set('Asia/Shanghai');
     }
 
